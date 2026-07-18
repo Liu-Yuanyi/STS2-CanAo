6c0ba31bc0cbfe6ec0e23248cf7e03ec25fb2d49
刘小康 <liu_xiaokang@stu.pku.edu.cn>
2026-07-18 15:22:31 +0800

    feat: implement fengwei wine and imperial edict vial potions

---
+++ src/CanAoNative/Potions/FengWeiJiuPotion.cs
@@ -0,0 +1,54 @@
+using CanAoNative.Powers;
+using CanAoNative.Rules.FengWei;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Potions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace CanAoNative.Potions;
+
+/// <summary>
+/// 凤威酒：获得 2 点凤威，在本回合获得 3 点凤威。
+/// </summary>
+public sealed class FengWeiJiuPotion : PotionModel
+{
+    public override PotionRarity Rarity => PotionRarity.Uncommon;
+    public override PotionUsage Usage => PotionUsage.CombatOnly;
+    public override TargetType TargetType => TargetType.Self;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<FengWeiPower>(2m),
+        new CardsVar(3)
+    ];
+
+    public override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<FengWeiPower>()
+    ];
+
+    protected override async Task OnUse(
+        PlayerChoiceContext choiceContext,
+        Creature? target)
+    {
+        Player owner = Owner
+            ?? throw new InvalidOperationException(
+                "FengWei Wine requires an owner.");
+
+        await FengWeiService.GainPermanent(
+            choiceContext,
+            owner,
+            DynamicVars["FengWeiPower"].BaseValue,
+            null);
+
+        await FengWeiService.ModifyTemporary(
+            choiceContext,
+            owner,
+            DynamicVars.Cards.BaseValue,
+            null);
+    }
+}
---
+++ src/CanAoNative/Potions/YuLingPingPotion.cs
@@ -0,0 +1,43 @@
+using CanAoNative.Cards;
+using CanAoNative.Rules.Edict;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Potions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace CanAoNative.Potions;
+
+/// <summary>
+/// 御令瓶：将 2 张诏令+加入手牌。
+/// </summary>
+public sealed class YuLingPingPotion : PotionModel
+{
+    public override PotionRarity Rarity => PotionRarity.Rare;
+    public override PotionUsage Usage => PotionUsage.CombatOnly;
+    public override TargetType TargetType => TargetType.Self;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new CardsVar(2)
+    ];
+
+    public override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromCard<EdictCard>()
+    ];
+
+    protected override async Task OnUse(
+        PlayerChoiceContext choiceContext,
+        Creature? target)
+    {
+        await EdictService.Generate(
+            choiceContext,
+            Owner,
+            DynamicVars.Cards.IntValue,
+            PileType.Hand,
+            upgraded: true);
+    }
+}
